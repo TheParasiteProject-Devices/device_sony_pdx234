8cae78d4bfc0c23cb5df2ca86248a974f3f5849b
Pranav Vashi <neobuddy89@gmail.com>
2022-06-01 02:13:54 +0530

    pdx234: libinit: set usb string
    * So that device shows proper name when connected via USB.
    
    Change-Id: I460f01b159b1d3dbd32c635e0d4bd08971574534
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- libinit/init_pdx234.cpp
+++ libinit/init_pdx234.cpp
@@ -129,6 +129,12 @@ void vendor_load_properties() {
     }
     full_property_override("build.product", "pdx234", false);
 
+    // Set hardware revision
+    property_override("ro.boot.hardware.revision", GetProperty("ro.boot.hwversion", "").c_str());
+    
+    // Set product name to show when connect through usb
+    property_override("vendor.usb.product_string", GetProperty("ro.product.marketname", "").c_str());
+
     /* Workaround CTS */
     workaround_cts_properties();
 
