275e543ca1ac122e9d3073548969916263ef8987
someone5678 <nemui3353@gmail.com>
2023-08-21 22:36:03 +0900

    pdx234: Parasitizing
    Change-Id: If7a0945e68ef0880cd8deed434e4ae92066fcdac

--- lineage_pdx234.mk
+++ lineage_pdx234.mk
@@ -21,6 +21,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.mk
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Inherit some common TheParasiteProject stuffs.
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_CALL_RECORDING := true
+TARGET_CHARGER_RESOURCE_COPY_OUT := $(TARGET_COPY_OUT_VENDOR)
+TARGET_INCLUDE_CARRIER_SERVICES := true
+TARGET_INCLUDE_CARRIER_SETTINGS := true
+TARGET_INCLUDE_PIXEL_EUICC := true
+TARGET_SUPPORTS_GOOGLE_BATTERY := false
+TARGET_GBOARD_KEY_HEIGHT := 1.1
+TARGET_EEA_V2_DEVICE := true
+TARGET_SUPPORTS_DSE_CHOICE_SCREEN := true
+TARGET_COLUMBUS_MODEL := crosshatch
+TARGET_BOOT_ANIMATION_RES := 1440
+
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
