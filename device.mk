b77c3b5655db6a7d079b009b6abb85cf50fce137
someone5678 <nemui3353@gmail.com>
2023-08-23 22:02:29 +0900

    pdx234: overlay: Wifi: Add empty wifi res overlay
    * Set target package to Google APEX
      since we ship prebuilt APEX
    
    Change-Id: I530b4805f227b75c50e4304fdfbdbe39144c2372

--- device.mk
+++ device.mk
@@ -42,6 +42,7 @@ PRODUCT_PACKAGES += \
     SonyPDX234SettingsRes \
     SonyPDX234SettingsProviderRes \
     SonyPDX234SystemUIRes \
+    SonyPdx234WifiRes \
     SonyPDX234FrameworksRes
 
 PRODUCT_PACKAGES += \
