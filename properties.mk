c14cc7302612a5fe095085ed96456b0a2cf2e7fd
someone5678 <someone5678@users.noreply.github.com>
2023-11-13 20:44:20 +0900

    pdx234: Move SOMC related props to pdx234
    Change-Id: Ie0ba921312e5449e3b5ec9dfbe046d4cd1377b29

--- properties.mk
+++ properties.mk
@@ -3,3 +3,12 @@ PRODUCT_VENDOR_PROPERTIES += \
 ro.surface_flinger.set_idle_timer_ms=4000 \
 ro.surface_flinger.set_touch_timer_ms=4000 \
 ro.surface_flinger.set_display_power_timer_ms=1000
+
+# SOMC
+PRODUCT_SYSTEM_PROPERTIES += \
+ro.semc.ms_type_id=PM-1430-BV \
+ro.semc.product.device=DQ \
+ro.semc.product.model=XQ-DQ72 \
+ro.semc.version.sw_revision=67.1.A.2.112 \
+ro.semc.version.sw_type=user \
+ro.semc.version.sw_variant=GLOBAL-C2
